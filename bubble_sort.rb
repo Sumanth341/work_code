4df76253c3fbcb7d48eed1329cb3b8c4f8e902a7
Rama Sangita <rasangita@copartadmins-MacBook-Pro-6.local>
2019-08-22 02:13:10 +0530

    bubble sort with recursion

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,13 +6,15 @@ a= Array.new
 for i in 0..length
     a[i]=gets.to_i
 end
-for j in 0..length
+def sort(length,a)
     length=length-1
     for i in 0..length
         a[i],a[i+1]=a[i+1],a[i] if a[i]>a[i+1]
     end
+    sort(length,a) if length!=0
 end
 puts "Sorted as below..."
+sort(length,a)
 puts a
 
 
